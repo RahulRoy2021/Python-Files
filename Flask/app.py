924c392d62d840af618ce1b81291289851084d5d
Rahul Roy <96610287+RahulRoy2021@users.noreply.github.com>
2024-04-10 10:08:00 +0530

    Update section deleted

--- Flask/app.py
+++ Flask/app.py
@@ -19,20 +19,5 @@ def submit():
     collection.insert_one({"username": username, "password": password})
     data = collection.find()
     return render_template("dashboard.html", data=data)
-
-@app.route('/update', methods=["GET","POST"])
-def update():
-    # Retrieve the form data
-    user_id = request.form.get("user_id")
-    new_username = request.form.get("new_username")
-    print("User ID:", user_id)  # Debugging print statement
-    print("New Username:", new_username)  # Debugging print statement
-
-    # Check if both user_id and new_username are present
-    if user_id and new_username:
-        collection.update_one({"_id": ObjectId(user_id)}, {"$set": {"username": new_username}})
-        return redirect('/')
-    else:
-        return "Error: User ID or new username missing."
 if __name__ == '__main__':
     app.run(debug=True)
